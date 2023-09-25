505d9cbd93b279e58b23ab9e330ee64a05290100
Jae-hong-Lee <keash8091@gmail.com>
2023-09-25 15:39:19 +0900

    Menu

--- main.js
+++ main.js
@@ -1,15 +1,84 @@
 const path = require("path");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
+const isDev = process.env.NODE_ENV !== "development";
+const isMac = process.platform === "darwin";
+
+// 메인 윈도우 만드는 함수
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "Image Resizer",
-    width: 500,
+    width: isDev ? 1000 : 500,
     height: 600,
   });
 
+  // 개발자모드로 실행시 개발자도구 오픈된 상태로 실행 env로 판단
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
+
+// about 윈도우 생성
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: "About Image Resizer",
+    width: 300,
+    height: 300,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
+// 앱이 준비됨
 app.whenReady().then(() => {
   createMainWindow();
+
+  // 메뉴구현
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+// 메뉴 템플릿
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: "fileMenu",
+  },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+];
+
+app.on("window-all-closed", () => {
+  if (!isMac) app.quit();
 });
---
+++ renderer/js/renderer.js
@@ -0,0 +1,39 @@
+// Some JavaScript to load the image and show the form. There is no actual backend functionality. This is just the UI
+// 이미지를 로드하고 폼을 보여주는 JS임. 백엔드 기능은 없고 단순 UI이 일뿐이다.
+const form = document.querySelector("#img-form");
+const img = document.querySelector("#img");
+const outputPath = document.querySelector("#output-path");
+const filename = document.querySelector("#filename");
+const heightInput = document.querySelector("#height");
+const widthInput = document.querySelector("#width");
+
+// 이미지 파일 로드
+function loadImage(e) {
+  const file = e.target.files[0];
+  // 이미지가 아닐경우 alert
+  if (!isFileImage(file)) {
+    alert("Please select an image file");
+    return;
+  }
+
+  // get original dimensions
+  const image = new Image();
+  image.src = URL.createObjectURL(file);
+  image.onload = function () {
+    widthInput.value = this.width;
+    heightInput.value = this.height;
+  };
+
+  form.style.display = "block";
+  // 파일 이름 표시
+  document.querySelector("#filename").innerHTML = file.name;
+}
+
+// 파일이 이미지인지 체크
+function isFileImage(file) {
+  const acceptedImageTypes = ["image/gif", "image/jpeg", "image/png"];
+  // 허용된 이미지 타입일 경우 return 'true'
+  return file && acceptedImageTypes.includes(file["type"]);
+}
+
+document.querySelector("#img").addEventListener("change", loadImage);
